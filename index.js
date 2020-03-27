af96e36fe2a2349640a3c30f242aa265dc9b7ede
Ankon Chakraborty <44019225+luciferankon@users.noreply.github.com>
2020-03-27 18:29:15 +0530

    ui with auth

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const getLabels = () => {
   const companyName = document.querySelector("#company-name").value;
-  fetch(`https://sector-tagging.appspot.com?companyName=${companyName}`)
+  const authKey = document.querySelector("#auth-key").value;
+  fetch(`https://sector-tagging.appspot.com?companyName=${companyName}`, {
+    headers: {
+      authorization: `Bearer ${authKey}`
+    }
+  })
     .then(res => res.json())
     .then(labels => {
       let labelsField = "<ul>";
@@ -21,10 +26,12 @@ const remove = () => {
   const label = event.target.parentElement.firstChild.data;
   event.target.parentElement.remove();
   const companyName = document.querySelector("#company-name").value;
+  const authKey = document.querySelector("#auth-key").value;
   fetch("https://sector-tagging.appspot.com/remove", {
     method: "POST",
     mode: "no-cors",
     headers: {
+      authorization: `Bearer ${authKey}`,
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
